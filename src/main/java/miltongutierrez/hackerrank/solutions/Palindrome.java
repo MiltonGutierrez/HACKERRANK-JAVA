90a13f9e436d4858de6cf87ec84b53be9f1b8e69
Milton Andres Gutierrez Lopez <80470554+MiltonGutierrez@users.noreply.github.com>
2025-04-05 10:53:23 -0500

    add: palindrome string solution

---
+++ src/main/java/miltongutierrez/hackerrank/solutions/Palindrome.java
@@ -0,0 +1,27 @@
+package miltongutierrez.hackerrank.solutions;
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        System.out.println(isPalindrome(A));
+        
+    }
+    
+    private static String isPalindrome(String str){
+        int i = 0;
+        char[] stringChars = str.toCharArray();
+        boolean isPalindrome = true;
+        int charLength = stringChars.length;
+        while(i < charLength - 1 -i){
+            if(stringChars[i] != stringChars[charLength -1 -i]){
+                isPalindrome = false;
+                break;
+             }
+            i++;
+        }
+        return isPalindrome ? "Yes" : "No";
+    }
+    
+}
